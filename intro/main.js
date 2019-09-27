c11090649bda7f250fd3a722d06fb180c04aa540
afridizx <afridizx@dukes.jmu.edu>
2019-09-26 22:53:03 -0400

    add specific product id to parent cart

--- intro/main.js
+++ intro/main.js
@@ -31,10 +31,6 @@ Vue.component('product', {
       <button v-on:click="addToCart"
           :disabled="!inStock"
           :class="{ disabledButton: !inStock }">Add to Cart</button>
-
-      <div class="cart">
-          <p>Cart({{ cart }})</p>
-      </div>
   </div>
 </div>
   `,
@@ -63,16 +59,15 @@ Vue.component('product', {
           variantQuantity: 0
         }
       ],
-      cart: 0
   }
 }, 
 methods: {
   addToCart() {
-    this.cart += 1
+    this.$emit('add-to-cart', this.variants[this.selectedVariant].variantId)
   },
   updateProduct(index) {
     this.selectedVariant = index
-  }
+  },
 },
 computed: {
   title() {
@@ -95,6 +90,12 @@ computed: {
 var app = new Vue({
     el: '#app',
     data: {
-      premium: true
+      premium: false,
+      cart: []
+    },
+    methods: {
+      updateCart(id) {
+        this.cart.push(id)
+      }
     }
 })
