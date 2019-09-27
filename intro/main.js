95da2cc493ebbb4b2117256dcbb7b4742152c68f
afridizx <afridizx@dukes.jmu.edu>
2019-09-26 22:24:07 -0400

    if inStock then no Add to Cart button

--- intro/main.js
+++ intro/main.js
@@ -3,8 +3,7 @@ var app = new Vue({
     data: {
         brand: 'Vue Mastery',
         product: 'Socks',
-        image: './assets/vmSocks-green.png',
-        inStock: true,
+        selectedVariant: 0,
         details: [
           "80% cotton",
           "20% polyester",
@@ -14,13 +13,15 @@ var app = new Vue({
           {
             variantId: 2234,
             variantColor: "green",
-            variantImage: './assets/vmSocks-green.png'
+            variantImage: './assets/vmSocks-green.png',
+            variantQuantity: 10
 
           },
           {
             variantId: 2235,
             variantColor: "blue",
-            variantImage: './assets/vmSocks-blue.png'
+            variantImage: './assets/vmSocks-blue.png',
+            variantQuantity: 0
           }
         ],
         cart: 0
@@ -29,8 +30,19 @@ var app = new Vue({
       addToCart() {
         this.cart += 1
       },
-      updateProduct(variantImage) {
-        this.image = variantImage
+      updateProduct(index) {
+        this.selectedVariant = index
+      }
+    },
+    computed: {
+      title() {
+        return this.brand + ' ' + this.product
+      },
+      image() {
+        return this.variants[this.selectedVariant].variantImage
+      },
+      inStock() {
+        return this.variants[this.selectedVariant].variantQuantity
       }
     }
 })
