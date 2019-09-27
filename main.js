6c60a6195afb0e18950e605cfdda5af1606fde28
afridizx <afridizx@dukes.jmu.edu>
2019-09-26 21:09:36 -0400

    add variant array of objects to data object

--- main.js
+++ main.js
@@ -2,6 +2,22 @@ var app = new Vue({
     el: '#app',
     data: {
         product: 'Socks',
-        image: './assets/vmSocks-green.png'
+        image: './assets/vmSocks-green.png',
+        inStock: true,
+        details: [
+          "80% cotton",
+          "20% polyester",
+          "Gender-neutral"
+        ],
+        variants: [
+          {
+            variantId: 2234,
+            variantColor: "green"
+          },
+          {
+            variantId: 2235,
+            variantColor: "blue"
+          }
+        ]
     }
 })
