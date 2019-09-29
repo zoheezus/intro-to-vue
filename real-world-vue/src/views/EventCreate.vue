8fdd40d357578b8adc296b29b4c26a17a312302b
afridizx <afridizx@dukes.jmu.edu>
2019-09-29 11:50:43 -0400

    event handling

--- real-world-vue/src/views/EventCreate.vue
+++ real-world-vue/src/views/EventCreate.vue
@@ -68,7 +68,14 @@ export default {
   },
   methods: {
     createEvent() {
-      this.$store.dispatch('createEvent', this.event)
+      this.$store
+        .dispatch('createEvent', this.event)
+        .then(() => {
+          this.event = this.createFreshEventObject()
+        })
+        .catch(() => {
+          console.log('There was a problem creating your event')
+        })
     },
     createFreshEventObject() {
       const user = this.$store.state.user
