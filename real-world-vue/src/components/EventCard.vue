f30e2bf32f6d703ac1a04b7be2a59508fa129d06
afridizx <afridizx@dukes.jmu.edu>
2019-09-28 23:26:18 -0400

    events and its styles

--- real-world-vue/src/components/EventCard.vue
+++ real-world-vue/src/components/EventCard.vue
@@ -1,6 +1,8 @@
 <template>
-  <div>
-    <h4>{{ title }}</h4>
+  <div class="event-card -shadow">
+    <span class="eyebrow">@{{ event.time }} on {{ event.date }}</span>
+    <h4 class="title">{{ event.title }}</h4>
+    <span>@{{ event.attendees.length }} attending</span>
   </div>
 </template>
 
@@ -8,14 +10,39 @@
 export default {
   data() {
     return {
-      title: 'Park Cleanup'
+      event: {
+        id: 1,
+        title: 'Mosque Cleanup',
+        date: 'Tues October 22, 2019',
+        time: '7:00',
+        atendees: [
+          { id: 'abc123', name: 'Zohaib Afridi' },
+          { id: 'def456', name: 'Hosam Hadid' }
+        ]
+      }
     }
   }
 }
 </script>
 
 <style scoped>
-h4 {
-  color: green;
+.event-card {
+  padding: 20px;
+  margin-bottom: 24px;
+  transition: all 0.2s linear;
+  cursor: pointer;
+}
+.event-card:hover {
+  transform: scale(1.01);
+  box-shadow: 0 3px 12px 0 rgba(0, 0, 0, 0.2), 0 1px 15px 0 rgba(0, 0, 0, 0.19);
+}
+.event-card > .title {
+  margin: 0;
+}
+
+.event-link {
+  color: black;
+  text-decoration: none;
+  font-weight: 100;
 }
 </style>
