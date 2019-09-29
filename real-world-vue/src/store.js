45c5952660916b74cfc8194fa389968ca30521b6
afridizx <afridizx@dukes.jmu.edu>
2019-09-29 11:44:34 -0400

    initial event creation through mutations and actions

--- real-world-vue/src/store.js
+++ real-world-vue/src/store.js
@@ -1,20 +1,34 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import EventService from '@/services/EventService.js'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
     user: { id: 'abc123', name: 'Adam Jahr' },
-    events: [
-      { id: 1, title: '...', organizer: '...' },
-      { id: 2, title: '...', organizer: '...' },
-      { id: 3, title: '...', organizer: '...' },
-      { id: 4, title: '...', organizer: '...' }
-    ]
+    categories: [
+      'sustainability',
+      'nature',
+      'animal welfare',
+      'housing',
+      'education',
+      'food',
+      'community'
+    ],
+    events: []
   },
-  mutations: {},
-  actions: {},
+  mutations: {
+    ADD_EVENT(state, event) {
+      state.events.push(event)
+    }
+  },
+  actions: {
+    createEvent({ commit }, event) {
+      EventService.postEvent(event)
+      commit('ADD_EVENT', event)
+    }
+  },
   getters: {
     getEventById: state => id => {
       return state.events.find(event => event.id === id)
