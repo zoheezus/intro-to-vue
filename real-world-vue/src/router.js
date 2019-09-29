dacce7efb94ec5280243c57f33f4a8e1472d0424
afridizx <afridizx@dukes.jmu.edu>
2019-09-28 21:58:41 -0400

    routing shtuff

--- real-world-vue/src/router.js
+++ real-world-vue/src/router.js
@@ -1,24 +1,27 @@
-import Vue from "vue";
-import Router from "vue-router";
-import Home from "./views/Home.vue";
+import Vue from 'vue'
+import Router from 'vue-router'
+import EventCreate from './views/EventCreate'
+import EventList from './views/EventList'
+import EventShow from './views/EventShow'
 
-Vue.use(Router);
+Vue.use(Router)
 
 export default new Router({
   routes: [
     {
-      path: "/",
-      name: "home",
-      component: Home
+      path: '/',
+      name: 'event-list',
+      component: EventList
     },
     {
-      path: "/about",
-      name: "about",
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () =>
-        import(/* webpackChunkName: "about" */ "./views/About.vue")
+      path: '/event',
+      name: 'event-show',
+      component: EventShow
+    },
+    {
+      path: '/event/create',
+      name: 'event-create',
+      component: EventCreate
     }
   ]
-});
+})
