3220eb88c1015aadde66d29cc5e4bff540e09ecb
afridizx <afridizx@dukes.jmu.edu>
2019-09-28 23:19:41 -0400

    styles

--- real-world-vue/src/components/EventCard.vue
+++ real-world-vue/src/components/EventCard.vue
@@ -14,4 +14,8 @@ export default {
 }
 </script>
 
-<style></style>
+<style scoped>
+h4 {
+  color: green;
+}
+</style>
--- real-world-vue/src/views/EventList.vue
+++ real-world-vue/src/views/EventList.vue
@@ -1,9 +1,20 @@
 <template>
   <div>
     <h1>Events Listing</h1>
+    <EventCard />
 
     <router-link :to="{ name: 'event-show', params: { id: '1' } }"
       >Show Event #1</router-link
     >
   </div>
 </template>
+
+<script>
+import EventCard from '@/components/EventCard.vue'
+
+export default {
+  components: {
+    EventCard
+  }
+}
+</script>
