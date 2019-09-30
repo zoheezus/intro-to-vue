2fb79cfacb75d871c78afa6a308362e0b9cc36fe
afridizx <afridizx@dukes.jmu.edu>
2019-09-30 04:16:36 -0400

    Login.vue setup

--- README.md
+++ README.md
@@ -2,3 +2,4 @@
 
 # Note:
   - **real-world-vue** contains updated and continued content from the **Intro to Vue.js** course, the **Real World Vue.js** course, the **Mastering Vuex** course, and the **Next-Level Vue** course.
+  - **vuetify-dashboard** folder contains contents from the **Beautify with Vuetify** course
--- vuetify-dashboard/src/App.vue
+++ vuetify-dashboard/src/App.vue
@@ -7,30 +7,7 @@
 			<v-btn text rounded>Login</v-btn>
 		</v-app-bar>
 		<v-content>
-			<!-- Login Module -->
-			<v-card width="400" class="mx-auto">
-				<v-card-title>
-					<h1 class="display-1">Login</h1>
-				</v-card-title>
-				<v-card-text>
-					<v-form>
-						<v-text-field label="Username" prepend-icon="mdi-account-circle" />
-						<v-text-field
-							:type="showPassword ? 'text' : 'password'"
-							label="Password"
-							prepend-icon="mdi-lock"
-							:append-icon="showPassword ? 'mdi-eye' : 'mdi-eye-off'"
-							@click:append="showPassword = !showPassword"
-						/>
-					</v-form>
-				</v-card-text>
-				<v-divider></v-divider>
-				<v-card-actions>
-					<v-btn color="success">Register</v-btn>
-					<v-spacer></v-spacer>
-					<v-btn color="info">Login</v-btn>
-				</v-card-actions>
-			</v-card>
+
 		</v-content>
 		<v-footer color="primary lighten-1" padless>
 			<v-row justify="center" no-gutters>
@@ -49,11 +26,7 @@ import HelloWorld from "./components/HelloWorld";
 
 export default {
 	name: "App",
-	components: {
-		HelloWorld
-	},
 	data: () => ({
-    showPassword: false,
     links: [
       'Home',
       'Login',
