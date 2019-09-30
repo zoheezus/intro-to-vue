9cffd45398e19d5dd9d96d91e5056a496708bfc6
afridizx <afridizx@dukes.jmu.edu>
2019-09-30 04:27:46 -0400

    refactoring

--- vuetify-dashboard/src/App.vue
+++ vuetify-dashboard/src/App.vue
@@ -3,15 +3,14 @@
 		<v-app-bar app color="primary" dark>
 			<v-toolbar-title>Vuetify Dashboard</v-toolbar-title>
 			<v-spacer></v-spacer>
-			<v-btn text rounded to="/">Home</v-btn>
-			<v-btn text rounded to="/login">Login</v-btn>
+			<v-btn v-for="link in links" :key="`${link.label}-header-link`" text rounded :to="link.url">{{ link.label }}</v-btn>
 		</v-app-bar>
 		<v-content>
 			<router-view></router-view>
 		</v-content>
 		<v-footer color="primary lighten-1" padless>
 			<v-row justify="center" no-gutters>
-				<v-btn v-for="link in links" :key="link" color="white" text rounded class="my-2">{{ link }}</v-btn>
+				<v-btn v-for="link in links" :key="`${link.label}-footer-link`" color="white" text rounded class="my-2">{{ link.label }}</v-btn>
 				<v-col class="primary lighten-2 py-4 text-center white--text" cols="12">
 					{{ new Date().getFullYear() }} —
 					<strong>Vuetify Dashboard</strong>
