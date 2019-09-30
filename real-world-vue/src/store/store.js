8c76a9cc2b3c6b033fa71578f62f0fe9e7d22097
afridizx <afridizx@dukes.jmu.edu>
2019-09-29 20:33:19 -0400

    setup event.js

---
+++ real-world-vue/src/store/store.js
@@ -0,0 +1,22 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+import * as user from '@/store/modules/user.js'
+
+Vue.use(Vuex)
+
+export default new Vuex.Store({
+  modules: {
+    user
+  },
+  state: {
+    categories: [
+      'sustainability',
+      'nature',
+      'animal welfare',
+      'housing',
+      'education',
+      'food',
+      'community'
+    ],
+  }
+})
