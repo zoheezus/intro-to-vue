3d57f6ee7a3e1832f7f1f768dc8955bd3ad3cbeb
afridizx <afridizx@dukes.jmu.edu>
2019-09-30 00:21:15 -0400

    404 page

---
+++ real-world-vue/src/views/NotFound.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>
+    <h>Oops!</h>
+    <h3>The page you're looking for is not here.</h3>
+    <router-link :to="{ name: 'event-list' }"
+      >Back to the home page</router-link
+    >
+  </div>
+</template>
