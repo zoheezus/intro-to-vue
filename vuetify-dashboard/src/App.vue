c9af5d3ab1bdb5b3c468790f420d427cd0da44e8
afridizx <afridizx@dukes.jmu.edu>
2019-09-30 04:06:28 -0400

    add navbar

--- vuetify-dashboard/src/App.vue
+++ vuetify-dashboard/src/App.vue
@@ -1,44 +1,63 @@
 <template>
-  <v-app>
-    <v-card width="400" class="mx-auto">
-      <v-card-title>
-        <h1 class="display-1">Login</h1>
-      </v-card-title>
-      <v-card-text>
-        <v-form>
-          <v-text-field
-            label="Username"
-            prepend-icon="mdi-account-circle"
-          />
-          <v-text-field
-            :type="showPassword ? 'text' : 'password'"
-            label="Password"
-            prepend-icon="mdi-lock"
-            :append-icon="showPassword ? 'mdi-eye' : 'mdi-eye-off'"
-            @click:append="showPassword = !showPassword"
-          />
-        </v-form>
-      </v-card-text>
-      <v-divider></v-divider>
-      <v-card-actions>
-        <v-btn color="success">Register</v-btn>
-        <v-spacer></v-spacer>
-        <v-btn color="info">Login</v-btn>
-      </v-card-actions>
-    </v-card>
-  </v-app>
+	<v-app>
+		<v-app-bar app color="primary" dark>
+			<v-toolbar-title>Vuetify Dashboard</v-toolbar-title>
+			<v-spacer></v-spacer>
+			<v-btn text rounded>Home</v-btn>
+			<v-btn text rounded>Login</v-btn>
+		</v-app-bar>
+		<v-content>
+			<!-- Login Module -->
+			<v-card width="400" class="mx-auto">
+				<v-card-title>
+					<h1 class="display-1">Login</h1>
+				</v-card-title>
+				<v-card-text>
+					<v-form>
+						<v-text-field label="Username" prepend-icon="mdi-account-circle" />
+						<v-text-field
+							:type="showPassword ? 'text' : 'password'"
+							label="Password"
+							prepend-icon="mdi-lock"
+							:append-icon="showPassword ? 'mdi-eye' : 'mdi-eye-off'"
+							@click:append="showPassword = !showPassword"
+						/>
+					</v-form>
+				</v-card-text>
+				<v-divider></v-divider>
+				<v-card-actions>
+					<v-btn color="success">Register</v-btn>
+					<v-spacer></v-spacer>
+					<v-btn color="info">Login</v-btn>
+				</v-card-actions>
+			</v-card>
+		</v-content>
+		<v-footer color="primary lighten-1" padless>
+			<v-row justify="center" no-gutters>
+				<v-btn v-for="link in links" :key="link" color="white" text rounded class="my-2">{{ link }}</v-btn>
+				<v-col class="primary lighten-2 py-4 text-center white--text" cols="12">
+					{{ new Date().getFullYear() }} —
+					<strong>Vuetify Dashboard</strong>
+				</v-col>
+			</v-row>
+		</v-footer>
+	</v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld';
+import HelloWorld from "./components/HelloWorld";
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld,
-  },
-  data: () => ({
-    showPassword: false
-  }),
+	name: "App",
+	components: {
+		HelloWorld
+	},
+	data: () => ({
+    showPassword: false,
+    links: [
+      'Home',
+      'Login',
+    ]
+	})
 };
 </script>
