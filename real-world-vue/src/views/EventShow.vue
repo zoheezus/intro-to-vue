72956a52f9ee2bf601e3b9255da82c6bb03d0154
afridizx <afridizx@dukes.jmu.edu>
2019-09-30 00:02:21 -0400

    more guard stuff

--- real-world-vue/src/views/EventShow.vue
+++ real-world-vue/src/views/EventShow.vue
@@ -14,9 +14,9 @@
     <p>{{ event.description }}</p>
     <h2>
       Attendees
-      <span class="badge -fill-gradient">{{
-        event.attendees ? event.attendees.length : 0
-      }}</span>
+      <span class="badge -fill-gradient">
+        {{ event.attendees ? event.attendees.length : 0 }}
+      </span>
     </h2>
     <ul class="list-group">
       <li
@@ -35,23 +35,17 @@
 </script>
 
 <script>
-import { mapState } from 'vuex'
 import NProgress from 'nprogress'
 import store from '@/store/store'
 
 export default {
-  props: ['id'],
-  beforeRouteEnter(routeTo, routeFrom, next) {
-    NProgress.start()
-    store.dispatch('event/fetchEvent', routeTo.params.id).then(() => {
-      NProgress.done()
-      next()
-    })
-  },
-  computed: mapState({
-    event: state => state.event.event
-  })
+  props: {
+    event: {
+      type: Object,
+      required: true
+    }
   }
+}
 </script>
 
 <style scoped>
